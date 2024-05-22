65609e55a409a637c4eaece2ccc2dccafe6bf2f2
vspahta <val.spahta@gmail.com>
2024-05-22 12:46:25 -0400

    Test repeatable script change

--- migrations/scripts/repeatable/R__fn_get_timezone.sql
+++ migrations/scripts/repeatable/R__fn_get_timezone.sql
@@ -10,3 +10,11 @@ $$
   return timezone;
 $$
 ;
+
+CREATE OR REPLACE FUNCTION DEMO.GET_DATE(business_date timestamp)
+RETURNS DATE
+LANGUAGE SQL
+AS
+$$
+    TO_DATE(SUBSTR(TO_CHAR(business_date),1,10))
+$$;
